92237887504a4d8a8a7c54b2118eb9c2980b82c7
Homer Strong <homer.strong@gmail.com>
2014-01-09 19:25:12 -0800

    move clojure dep to provided profile

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "https://github.com/guilespi/clj-zipkin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [thrift-clj "0.2.1"]
+  :dependencies [[thrift-clj "0.2.1"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.clojure/data.codec "0.1.0"]
                  [clj-scribe "0.3.1"]
@@ -12,7 +11,8 @@
   :test-paths ["test" "examples"]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
                                   [compojure "1.1.6"]
-                                  [ring "1.2.1"]]}}
+                                  [ring "1.2.1"]]}
+             :provided {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :plugins [[lein-thriftc "0.1.0"]]
   :prep-tasks ["thriftc"]
   :thriftc {:javac-opts ["-source" "1.6" "-target" "1.6"]}
