e6f8b2853704f589025ebe107b40d40fff5b267d
Homer Strong <homer.strong@gmail.com>
2014-01-09 19:21:30 -0800

    bump thrift-clj dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [thrift-clj "0.1.1"]
+                 [thrift-clj "0.2.1"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.clojure/data.codec "0.1.0"]
                  [clj-scribe "0.3.1"]
